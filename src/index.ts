6e2aa00700a27db7e3ededc92514acf307e77f25
Mai Ngoc Doan <maidoan2002ns@gmail.com>
2023-04-17 09:03:44 +0000

    Update 5 files

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,15 @@ import express from "express";
 import cors from "cors";
 import mysql from "mysql";
 import dotenv from "dotenv";
-import loginRoute from "../src/api/loginapi/loginroute";
+import loginRoute from "../src/api/loginapi/login.router";
 dotenv.config();
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use("/login", loginRoute);
-app.get("/", (req, res) => {
-  res.send({ message: "Hello. My name is Doan" });
+app.use("/", (req, res) => {
+  res.send({ message: "Hello worlddd" });
 });
 
 app.listen(3000);
